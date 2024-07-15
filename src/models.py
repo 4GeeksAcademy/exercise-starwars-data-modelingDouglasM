b4d31b76057d53a1001fa917f1bb4fbbc80f1222
Douglas Melendez <160060653+MrDoux@users.noreply.github.com>
2024-07-15 16:15:44 +0000

    se ha corregido las tablas

--- src/models.py
+++ src/models.py
@@ -7,51 +7,36 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-
-usuario_personaje = Table('usuario_personaje', Base.metadata,
-    Column('usuario_id', Integer, ForeignKey('usuario.id'), primary_key=True),
-    Column('personaje_id', Integer, ForeignKey('personaje.id'), primary_key=True)
-)
-
-
-usuario_planeta = Table('usuario_planeta', Base.metadata,
-    Column('usuario_id', Integer, ForeignKey('usuario.id'), primary_key=True),
-    Column('planeta_id', Integer, ForeignKey('planeta.id'), primary_key=True)
-)
-
 class Usuario(Base):
     __tablename__ = 'usuario'
-    id = Column(Integer, primary_key=True)
+    usuario_id = Column(Integer, primary_key=True)
     nombre = Column(String(250), nullable=False)
     apellido = Column(String(250), nullable=False)
     email = Column(String(250), nullable=False, unique=True)
     password = Column(String(250), nullable=False)
     fecha_suscripcion = Column(String(250))
 
-    personajes = relationship('Personaje', secondary=usuario_personaje, back_populates='usuarios')
-    planetas = relationship('Planeta', secondary=usuario_planeta, back_populates='usuarios')
+  
 
 class Personaje(Base):
     __tablename__ = 'personaje'
-    id = Column(Integer, primary_key=True)
+    personaje_id = Column(Integer, primary_key=True)
     nombre = Column(String(250), nullable=False)
     genero = Column(String(250))
     especie = Column(String(250))
 
-    usuarios = relationship('Usuario', secondary=usuario_personaje, back_populates='personajes')
 
 class Planeta(Base):
     __tablename__ = 'planeta'
-    id = Column(Integer, primary_key=True)
+    planeta_id = Column(Integer, primary_key=True)
     nombre = Column(String(250), nullable=False)
     clima = Column(String(250))
     terreno = Column(String(250))
 
-    usuarios = relationship('Usuario', secondary=usuario_planeta, back_populates='planetas')
 
 class Favorito(Base):
     __tablename__ = 'favorito'
-    id = Column(Integer, primary_key=True)
+    favorito_id = Column(Integer, primary_key=True)
     usuario_id = Column(Integer, ForeignKey('usuario.id'), nullable=False)
     personaje_id = Column(Integer, ForeignKey('personaje.id'))
     planeta_id = Column(Integer, ForeignKey('planeta.id'))
